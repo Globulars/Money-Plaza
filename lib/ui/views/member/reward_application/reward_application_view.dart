355a423f5efc75ecce0a0c9f67db84f299bd7825
Khawar Ayoub <124525015+Khawar101@users.noreply.github.com>
2023-06-18 18:20:03 +0500

    fr

--- lib/ui/views/member/reward_application/reward_application_view.dart
+++ lib/ui/views/member/reward_application/reward_application_view.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: avoid_types_as_parameter_names, non_constant_identifier_names, prefer_const_literals_to_create_immutables
+// ignore_for_file: non_constant_identifier_names, avoid_types_as_parameter_names, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 import 'package:money_plaza/ui/common/app_icons.dart';
