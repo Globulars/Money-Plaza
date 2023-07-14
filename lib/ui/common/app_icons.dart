09e42ab4a410daa8b65cf9228e9b862f0e1958ce
Rehan Ullah <rehan.gee888@gmail.com>
2023-07-14 16:25:43 +0500

    Icons Class

--- lib/ui/common/app_icons.dart
+++ lib/ui/common/app_icons.dart
@@ -1,13 +1,52 @@
-const String logo = 'assets/icons/Landing_icons/Money-Plaza.png';
-const String loans = 'assets/icons/Landing_icons/Loans.png';
+class myIcons{
 
-// const String logo1 = '';
-// const String logo1 = '';
-// const String logo1 = '';
-// const String logo1 = '';
-// const String logo1 = '';
-// const String logo1 = '';
-// const String logo1 = '';
-// const String logo1 = '';
-// const String logo1 = '';
-// const String logo1 = '';
+// Credit Card Frames
+static const String creditCardFrame = 'assets/icons/credit-cards/Frame 230.png';
+
+//Landing Icons/Contact_Icons
+static const String callLog = 'assets/icons/Landing_icons/contacts_icon/call log.png';
+static const String cancel = 'assets/icons/Landing_icons/contacts_icon/cancel.png';
+static const String contacts = 'assets/icons/Landing_icons/contacts_icon/Contact.png';
+static const String mail = 'assets/icons/Landing_icons/contacts_icon/mail.png';
+static const String tele = 'assets/icons/Landing_icons/contacts_icon/tele.png';
+static const String wts = 'assets/icons/Landing_icons/contacts_icon/wts.png';
+
+//Landing Icons
+static const String appbarHome = 'assets/icons/Landing_icons/appbar-home.png';
+static const String appbarSetting = 'assets/icons/Landing_icons/appbar-setting.png';
+static const String appbarTitle = 'assets/icons/Landing_icons/appbar-title.png';
+static const String regularUser = 'assets/icons/Landing_icons/fa-regular_user.png';
+static const String loader = 'assets/icons/Landing_icons/loader.png';
+static const String loans = 'assets/icons/Landing_icons/Loans.png';
+static const String moneyPlaza1 = 'assets/icons/Landing_icons/Money-Plaza.png';
+static const String morgages = 'assets/icons/Landing_icons/Morgages.png';
+static const String returnIcon = 'assets/icons/Landing_icons/return_icon.png';
+static const String setting = 'assets/icons/Landing_icons/setting.png';
+static const String svg = 'assets/icons/Landing_icons/svg.png';
+
+//Loans Icons
+
+static const String aiMatching = 'assets/icons/loans-icons/A-I-matching.png';
+static const String apply = 'assets/icons/loans-icons/apply.png';
+static const String calculator = 'assets/icons/loans-icons/calculator.png';
+static const String cilSortDecending = 'assets/icons/loans-icons/cil_sort-descending.png';
+static const String commericalLoan = 'assets/icons/loans-icons/commerical-loan.png';
+static const String compare1 = 'assets/icons/loans-icons/compare.png';
+static const String compare2 = 'assets/icons/loans-icons/compare2.png';
+static const String contactInfo = 'assets/icons/loans-icons/contact-info.png';
+static const String detail = 'assets/icons/loans-icons/detail.png';
+static const String done = 'assets/icons/loans-icons/done.png';
+static const String filter = 'assets/icons/loans-icons/filter.png';
+static const String balanceTrans = 'assets/icons/loans-icons/loan-balance-trans.png';
+static const String match = 'assets/icons/loans-icons/match.png';
+static const String moneyPlaza2 = 'assets/icons/loans-icons/money-plaza.png';
+static const String next = 'assets/icons/loans-icons/next.png';
+static const String personalLoans = 'assets/icons/loans-icons/personal-loans.png';
+static const String previous = 'assets/icons/loans-icons/pervious.png';
+static const String promise = 'assets/icons/loans-icons/promise.png';
+static const String ownerLoan = 'assets/icons/loans-icons/property-owner-loan.png';
+static const String weLend = 'assets/icons/loans-icons/we-lend.png';
+
+
+
+}
