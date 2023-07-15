d87dc9666c197301b507986c283e8016f6e065d8
mudassir060 <mudassirmukhtar4@gmail.com>
2023-07-15 18:00:59 +0500

    bottom bar

--- lib/ui/views/landing/landing_view.dart
+++ lib/ui/views/landing/landing_view.dart
@@ -20,96 +20,101 @@ class LandingView extends StackedView<LandingViewModel> {
     Widget? child,
   ) {
     return Scaffold(
-      appBar: appBar(() {}, () {}),
-      body: Container(
-        width: MediaQuery.of(context).size.width,
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage("assets/screens/mainbg.png"), fit: BoxFit.fill),
-        ),
-        child: Column(children: [
-          Image.asset(
-            "assets/screens/landing/cover_image.png",
-            fit: BoxFit.fill,
-            height: 180,
+        appBar: appBar(() {}, () {}),
+        body: Container(
+          width: MediaQuery.of(context).size.width,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("assets/screens/mainbg.png"),
+                fit: BoxFit.fill),
           ),
-          Padding(
-            padding: const EdgeInsets.all(30.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    IconBoxBtn(
-                      height: 110,
-                      width: 120,
-                      color: kcDarkGreyColor,
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                      text: "Loans",
-                      btmimage: myIcons.loans,
-                      imgwidth: 50,
-                    ),
-                    horizontalSpaceSmall,
-                    IconBoxBtn(
-                      height: 110,
-                      width: 120,
-                      color: kcDarkGreyColor,
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                      text: "Morgages",
-                      btmimage: myIcons.morgages,
-                      imgwidth: 50,
-                    ),
-                  ],
-                ),
-                verticalSpaceSmall,
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    IconBoxBtn(
-                      height: 110,
-                      width: 120,
-                      color: kcDarkGreyColor,
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                      text: "Insurances",
-                      btmimage: myIcons.svg,
-                      imgwidth: 50,
-                    ),
-                    horizontalSpaceSmall,
-                    IconBoxBtn(
-                      height: 110,
-                      width: 120,
-                      color: kcDarkGreyColor,
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                      text: "Credit Cards",
-                      btmimage: myIcons.creditCard,
-                      imgwidth: 50,
-                    ),
-                  ],
-                ),
-                verticalSpaceSmall,
-                IconBoxBtn(
-                  height: 110,
-                  width: 120,
-                  color: kcDarkGreyColor,
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                  text: "Sign Up/Login",
-                  btmimage: myIcons.regularUser,
-                  imgwidth: 50,
-                ),
-              ],
+          child: Column(children: [
+            Image.asset(
+              "assets/screens/landing/cover_image.png",
+              fit: BoxFit.fill,
+              height: 180,
             ),
-          ),
-        ]),
-      ),
-      bottomNavigationBar: bottomBar(ReturnButton(text: "Return",imageLeft: myIcons.returnIcon1,))
-    );
+            Padding(
+              padding: const EdgeInsets.all(30.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      IconBoxBtn(
+                        height: 110,
+                        width: 120,
+                        color: kcDarkGreyColor,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        text: "Loans",
+                        btmimage: myIcons.loans,
+                        imgwidth: 50,
+                      ),
+                      horizontalSpaceSmall,
+                      IconBoxBtn(
+                        height: 110,
+                        width: 120,
+                        color: kcDarkGreyColor,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        text: "Morgages",
+                        btmimage: myIcons.morgages,
+                        imgwidth: 50,
+                      ),
+                    ],
+                  ),
+                  verticalSpaceSmall,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      IconBoxBtn(
+                        height: 110,
+                        width: 120,
+                        color: kcDarkGreyColor,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        text: "Insurances",
+                        btmimage: myIcons.svg,
+                        imgwidth: 50,
+                      ),
+                      horizontalSpaceSmall,
+                      IconBoxBtn(
+                        height: 110,
+                        width: 120,
+                        color: kcDarkGreyColor,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        text: "Credit Cards",
+                        btmimage: myIcons.creditCard,
+                        imgwidth: 50,
+                      ),
+                    ],
+                  ),
+                  verticalSpaceSmall,
+                  IconBoxBtn(
+                    height: 110,
+                    width: 120,
+                    color: kcDarkGreyColor,
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    text: "Sign Up/Login",
+                    btmimage: myIcons.regularUser,
+                    imgwidth: 50,
+                  ),
+                ],
+              ),
+            ),
+          ]),
+        ),
+        bottomNavigationBar: bottomBar(ReturnButton(
+          text: "Return",
+          imageLeft: myIcons.returnIcon1,
+          height: 30.0,
+          width: 100.0,
+        )));
   }
 
   @override
--- lib/ui/widgets/bottom_bar.dart
+++ lib/ui/widgets/bottom_bar.dart
@@ -5,12 +5,13 @@ Widget bottomBar(widget) {
   return Padding(
     padding: const EdgeInsets.all(8.0),
     child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
         Image.asset(
           myIcons.contacts,
           height: 50,
         ),
-        widget()
+        widget
       ],
     ),
   );
