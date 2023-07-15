f994f2626ae0d9af688668d55f70f9d48785f430
Khawar Ayoub <124525015+Khawar101@users.noreply.github.com>
2023-07-15 23:13:24 +0500

    ..

--- lib/ui/views/loan/loan_viewmodel.dart
+++ lib/ui/views/loan/loan_viewmodel.dart
@@ -1,12 +1,10 @@
-import 'package:money_plaza/app/app.router.dart';
-import 'package:stacked/stacked.dart';
-import 'package:stacked_services/stacked_services.dart';
 
-import '../../../app/app.locator.dart';
+import 'package:stacked/stacked.dart';
+
 
 class LoanViewModel extends BaseViewModel {
 
-  final _navigationService = locator<NavigationService>();
+  // final _navigationService = locator<NavigationService>();
 
   //  navigateCalculatorDialog() {
   //   _navigationService.navigateToCalculatorDialog();
