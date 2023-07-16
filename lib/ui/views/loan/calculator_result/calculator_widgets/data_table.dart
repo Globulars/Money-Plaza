8866d1085dc554c43efdffad4be18c92d5f93e64
Khawar Ayoub <124525015+Khawar101@users.noreply.github.com>
2023-07-16 17:25:31 +0500

    ...

--- lib/ui/views/loan/calculator_result/calculator_widgets/data_table.dart
+++ lib/ui/views/loan/calculator_result/calculator_widgets/data_table.dart
@@ -18,13 +18,15 @@ class CalDataTable extends ViewModelWidget<CalculatorResultViewModel> {
           scrollDirection: Axis.horizontal,
           child: DataTable(
           columnSpacing: 3,
-         dataRowMinHeight: 2,
-         dataRowMaxHeight: 3,
+         dataRowMinHeight: 3,
+         dataRowMaxHeight: 20,
+         horizontalMargin: 0,
+         
             columns: [
               // Set the name of the column
               DataColumn(label: SizedBox(
                 width: 40,
-                child: CustomText(text:'Period',textAlign: TextAlign.end,color: darkGreenHeigh,fontSize: 10,)),),
+                child: CustomText(text:'Period',textAlign: TextAlign.start,color: darkGreenHeigh,fontSize: 10,)),),
               DataColumn(label: SizedBox(
                 width: 60,
                 // height: 200,
@@ -37,14 +39,14 @@ class CalDataTable extends ViewModelWidget<CalculatorResultViewModel> {
             rows:[
               // Set the values to the columns 
               DataRow(cells: [
-                DataCell(SizedBox( width: 30,child: CustomText(text:'1',textAlign: TextAlign.center,fontSize: 10,))),
+                DataCell(SizedBox( width: 20,child: CustomText(text:'1',textAlign: TextAlign.center,fontSize: 10,))),
                 DataCell(SizedBox(width: 60,child: Center(child:CustomText(text:"\$6.36.777",textAlign: TextAlign.center,fontSize: 8,)))),
                 DataCell(SizedBox(width: 60,child: Center(child:CustomText(text:"\$65.36.777",textAlign: TextAlign.center,fontSize: 8,)))),
                 DataCell(SizedBox(width: 60,child: Center(child:CustomText(text:"\$65.36.777",textAlign: TextAlign.center,fontSize: 8,)))),
                  DataCell(SizedBox(width: 60,child: Center(child:CustomText(text:"\$654.36.777",textAlign: TextAlign.center,fontSize: 8,)))),
               ]),
               DataRow(cells: [
-                DataCell(SizedBox( width: 30,child: CustomText(text:'2',textAlign: TextAlign.center,fontSize: 10,))),
+                DataCell(SizedBox( width: 20,child: CustomText(text:'2',textAlign: TextAlign.center,fontSize: 10,))),
                 DataCell(SizedBox(width: 60,child: Center(child:CustomText(text:"\$6.36.777",textAlign: TextAlign.center,fontSize: 8)))),
                 DataCell(SizedBox(width: 60,child: Center(child:CustomText(text:"\$65.36.777",textAlign: TextAlign.center,fontSize: 8,)))),
                 DataCell(SizedBox(width: 60,child: Center(child:CustomText(text:"\$65.36.777",textAlign: TextAlign.center,fontSize: 8,)))),
