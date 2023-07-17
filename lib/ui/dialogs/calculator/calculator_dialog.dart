eb817a90d8a4f86a475b76620534bb9880b53018
mudassir060 <mudassirmukhtar4@gmail.com>
2023-07-17 18:11:25 +0500

    calculator dilog box

--- lib/ui/dialogs/calculator/calculator_dialog.dart
+++ lib/ui/dialogs/calculator/calculator_dialog.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:money_plaza/ui/common/app_colors.dart';
+import 'package:money_plaza/ui/common/app_icons.dart';
 import 'package:money_plaza/ui/common/ui_helpers.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 
+import '../../widgets/common/custom_text_field/custom_text_field.dart';
+import '../../widgets/common/icon_box_btn/return_button.dart';
+import '../../widgets/common/icon_box_btn/sub_bar.dart';
+import '../../widgets/common/icon_box_btn/submit_button.dart';
+import '../../widgets/common/icon_box_btn/text.dart';
 import 'calculator_dialog_model.dart';
 
 const double _graphicSize = 60;
@@ -24,80 +30,137 @@ class CalculatorDialog extends StackedView<CalculatorDialogModel> {
     CalculatorDialogModel viewModel,
     Widget? child,
   ) {
+    final width = MediaQuery.of(context).size.width;
+
     return Dialog(
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
       backgroundColor: Colors.white,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Expanded(
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        request.title ?? 'Hello Stacked Dialog!!',
-                        style: const TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.w900,
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 0),
+          child: Column(
+            children: [
+              SubBar(
+                height: 40,
+                color: Colors.white,
+                width: width * 1,
+                image: myIcons.calculator,
+                text: 'Interest Calculator',
+                fontSize: 16,
+                fontWeight: FontWeight.w500,
+                btmLeftRadius: 0,
+                btmRightRadius: 0,
+                topimage: myIcons.cancel,
+              ),
+              verticalSpaceTiny,
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 6, vertical: 6),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    CustomText(
+                      text: 'Borrowing Amount',
+                      fontWeight: FontWeight.w600,
+                    ),
+                    CustomTextField(
+                      hintText: 'HK\$',
+                      height: 40,
+                    ),
+                    verticalSpaceTiny,
+                    CustomText(
+                      text: 'Repayment Method',
+                      fontWeight: FontWeight.w600,
+                    ),
+                    verticalSpaceTiny,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        ReturnButton(
+                          height: 40,
+                          text: 'Fixed Rate Term Loans',
+                          width: width * 0.42,
                         ),
-                      ),
-                      if (request.description != null) ...[
-                        verticalSpaceTiny,
-                        Text(
-                          request.description!,
-                          style: const TextStyle(
-                            fontSize: 14,
-                            color: kcMediumGrey,
-                          ),
-                          maxLines: 3,
-                          softWrap: true,
+                        SubmitButton(
+                          height: 40,
+                          text: 'Resolving Loans',
+                          width: width * 0.32,
+                        )
+                      ],
+                    ),
+                    verticalSpace(5),
+                    ReturnButton(
+                      height: 40,
+                      text: 'Prepaaid Interest(Commerical Loans)',
+                      width: width * 1,
+                    ),
+                    verticalSpaceTiny,
+                    CustomText(
+                      text: 'Calculation Items',
+                      fontWeight: FontWeight.w600,
+                    ),
+                    verticalSpaceTiny,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        ReturnButton(
+                          height: 40,
+                          text: 'Tenor',
+                          width: width * 0.36,
+                        ),
+                        SubmitButton(
+                          height: 40,
+                          text: 'APR',
+                          width: width * 0.36,
+                        )
+                      ],
+                    ),
+                    verticalSpace(5),
+                    ReturnButton(
+                      height: 40,
+                      text: 'Monthly Repayment Amount',
+                      width: width * 1,
+                    ),
+                    verticalSpaceTiny,
+                    verticalSpaceTiny,
+                    CustomText(
+                      text: 'MonthlyRepayment Amount',
+                      fontWeight: FontWeight.w600,
+                    ),
+                    CustomTextField(
+                      hintText: 'HK\$',
+                      height: 40,
+                    ),
+                    verticalSpaceTiny,
+                    CustomText(
+                      text: 'Interest Rate',
+                      fontWeight: FontWeight.w500,
+                    ),
+                    CustomTextField(
+                      hintText: '%',
+                      height: 40,
+                    ),
+                    verticalSpaceSmall,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        SubmitButton(
+                          boxColor: Colors.transparent,
+                          image: myIcons.iconPowerReset,
+                          text: 'Reset all',
+                          color: darkGreenLight,
+                        ),
+                        SubmitButton(
+                          text: 'Calculate Now',
+                          height: 40,
+                          width: 120,
                         ),
                       ],
-                    ],
-                  ),
-                ),
-                Container(
-                  width: _graphicSize,
-                  height: _graphicSize,
-                  decoration: const BoxDecoration(
-                    color: Color(0xFFF6E7B0),
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(_graphicSize / 2),
-                    ),
-                  ),
-                  alignment: Alignment.center,
-                  child: const Text('⭐️', style: TextStyle(fontSize: 30)),
-                )
-              ],
-            ),
-            verticalSpaceMedium,
-            GestureDetector(
-              onTap: () => completer(DialogResponse(confirmed: true)),
-              child: Container(
-                height: 50,
-                width: double.infinity,
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-                  color: Colors.black,
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: const Text(
-                  'Got it',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 16,
-                  ),
+                    )
+                  ],
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
