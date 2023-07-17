27d0cad20896dcff6dab459b6ee8cd08777d81be
Khawar Ayoub <124525015+Khawar101@users.noreply.github.com>
2023-07-17 15:47:52 +0500

    loan screen complete

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
               bodyColor: Colors.black,
             ),
       ),
-      initialRoute: Routes.commericalLoanView,
+      initialRoute: Routes.commericalResultView,
       onGenerateRoute: StackedRouter().onGenerateRoute,
       navigatorKey: StackedService.navigatorKey,
       navigatorObservers: [
--- lib/ui/views/loan/commerical_loan/commerical_result/commerical_result_view.dart
+++ lib/ui/views/loan/commerical_loan/commerical_result/commerical_result_view.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
-
+import 'package:money_plaza/ui/common/app_icons.dart';
+import '../../../../common/ui_helpers.dart';
+import '../../../../widgets/app_bar.dart';
+import '../../../../widgets/common/result_card.dart';
+import '../../../../widgets/common/topbar_loan_result.dart';
 import 'commerical_result_viewmodel.dart';
 
 class CommericalResultView extends StackedView<CommericalResultViewModel> {
@@ -12,11 +16,48 @@ class CommericalResultView extends StackedView<CommericalResultViewModel> {
     CommericalResultViewModel viewModel,
     Widget? child,
   ) {
-    return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.background,
-      body: Container(
-        padding: const EdgeInsets.only(left: 25.0, right: 25.0),
-      ),
+    final width = MediaQuery.of(context).size.width;
+    return Stack(
+      children: [
+        Container(
+          width: width * 1,
+          height: MediaQuery.of(context).size.height * 1,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage(myIcons.backgroundimage), fit: BoxFit.fill),
+          ),
+        ),
+        DefaultTabController(
+          length: 3,
+          child: Scaffold(
+            backgroundColor: Colors.transparent,
+            appBar: appBar(),
+            body:  Column(
+              children: [
+                resultLoanTop(context),
+                verticalSpaceSmall,
+              Expanded(
+                    child: SingleChildScrollView(
+                        child: ListView.builder(
+                  itemCount: 3,
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemBuilder: (BuildContext context, int index) {
+                    return resultCard(context);
+                  },
+                ))),
+              ],
+            ),
+            // bottomNavigationBar: Container(
+            //     height: 60,
+            //     width: width * 1,
+            //     child: SizedBox(
+            //         child: Image.asset(
+            //       myIcons.contacts,
+            //     ))),
+          ),
+        ),
+      ],
     );
   }
 
