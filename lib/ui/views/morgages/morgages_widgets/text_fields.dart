46d0087286dfecdb87e9f6f0753cfd34fcfd0fd3
Khawar Ayoub <124525015+Khawar101@users.noreply.github.com>
2023-07-17 18:46:54 +0500

    ..

--- lib/ui/views/morgages/morgages_widgets/text_fields.dart
+++ lib/ui/views/morgages/morgages_widgets/text_fields.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: avoid_types_as_parameter_names, non_constant_identifier_names, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
 import '../../../common/ui_helpers.dart';
@@ -14,7 +16,6 @@ class MorgTextFields extends ViewModelWidget<MorgagesViewModel> {
     BuildContext context,
     MorgagesViewModel viewModel,
   ) {
-    final width = MediaQuery.of(context).size.width;
     return Expanded(
       child: SingleChildScrollView(
         child: Padding(
