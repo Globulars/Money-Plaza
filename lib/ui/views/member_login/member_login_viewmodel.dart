c460ccc64478ada9cf5961eb3527edb47ebf0abd
mudassir060 <mudassirmukhtar4@gmail.com>
2023-07-17 18:35:16 +0500

    connect dilog box with member

--- lib/ui/views/member_login/member_login_viewmodel.dart
+++ lib/ui/views/member_login/member_login_viewmodel.dart
@@ -1,3 +1,15 @@
 import 'package:stacked/stacked.dart';
+import 'package:stacked_services/stacked_services.dart';
 
-class MemberLoginViewModel extends BaseViewModel {}
+import '../../../app/app.dialogs.dart';
+import '../../../app/app.locator.dart';
+
+class MemberLoginViewModel extends BaseViewModel {
+    final _dialogService = locator<DialogService>();
+
+    void showResetPassword() {
+    _dialogService.showCustomDialog(
+      variant: DialogType.resetPassword,
+    );
+  }
+}
