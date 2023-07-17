1bc038534a7b4a8bed0695fd2e7945431b70556e
mudassir060 <mudassirmukhtar4@gmail.com>
2023-07-17 12:47:05 +0500

    landing to setting page

--- lib/ui/views/landing/landing_viewmodel.dart
+++ lib/ui/views/landing/landing_viewmodel.dart
@@ -1,3 +1,14 @@
+import 'package:money_plaza/app/app.router.dart';
 import 'package:stacked/stacked.dart';
+import 'package:stacked_services/stacked_services.dart';
 
-class LandingViewModel extends BaseViewModel {}
+import '../../../app/app.locator.dart';
+
+class LandingViewModel extends BaseViewModel {
+  final _navigationService = locator<NavigationService>();
+
+  Future navigateToSettings() async {
+    await Future.delayed(const Duration(seconds: 3));
+    _navigationService.navigateToSettingsView();
+  }
+}
