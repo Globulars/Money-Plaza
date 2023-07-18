e79da9d25bf12a9f4e4b7ad5d816479fa1513619
Rehan Ullah <rehan.gee888@gmail.com>
2023-07-18 16:48:07 +0500

    Reset Button Complete

---
+++ lib/ui/dialogs/reset_password/ResetPassTab.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:money_plaza/ui/dialogs/reset_password/reset_password_dialog_model.dart';
+import 'package:money_plaza/ui/dialogs/reset_password/reset_with_email.dart';
+import 'package:stacked/stacked.dart';
+
+import '../../common/app_colors.dart';
+
+class ResetPassTab extends StackedView<ResetPasswordDialogModel> {
+  const ResetPassTab({Key? key}) : super(key: key);
+
+  @override
+  Widget builder(
+    BuildContext context,
+    ResetPasswordDialogModel viewModel,
+    Widget? child,
+  ) {
+    final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
+    return DefaultTabController(
+      length: 2,
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: SizedBox(
+          height: height*0.35,
+          width: width,
+          child: Scaffold(
+            backgroundColor: Colors.transparent,
+            body: NestedScrollView(
+              headerSliverBuilder:
+                  (BuildContext context, bool innerBoxIsScrolled) {
+                return <Widget>[
+                  SliverAppBar(
+                    toolbarHeight: 0,
+                    backgroundColor: Colors.transparent,
+                    pinned: false,
+                    floating: false,
+                    shape: const RoundedRectangleBorder(
+                      side: BorderSide(width: 1),
+                    ),
+                    bottom: TabBar(
+                      unselectedLabelColor: kcPureBlack,
+                      labelColor: kcPureWhite,
+                      labelStyle: GoogleFonts.ibmPlexSans(fontSize: 10),
+                      indicator: const BoxDecoration(
+                          // Creates border
+                          color: darkGreenHeigh),
+                      tabs: [
+                        Tab(
+                          child: Text(
+                            "Rest by Email",
+                            style: GoogleFonts.ibmPlexSans(fontSize: 14),
+                          ),
+                        ),
+                        Tab(
+                          child: Text(
+                            "Rest by Mobile",
+                            style: GoogleFonts.ibmPlexSans(fontSize: 14),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ];
+              },
+              body: const TabBarView(
+                children: <Widget>[
+                  ResetWithEmail(),
+                  Text("Register With Mobile Container"),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  ResetPasswordDialogModel viewModelBuilder(
+    BuildContext context,
+  ) =>
+      ResetPasswordDialogModel();
+}
--- lib/ui/dialogs/reset_password/reset_password_dialog.dart
+++ lib/ui/dialogs/reset_password/reset_password_dialog.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:money_plaza/ui/common/app_colors.dart';
 import 'package:money_plaza/ui/common/app_icons.dart';
-import 'package:money_plaza/ui/common/ui_helpers.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
-
-import '../../views/member_login/login_with_email.dart';
+import '../../widgets/common/icon_box_btn/sub_bar.dart';
+import 'ResetPassTab.dart';
 import 'reset_password_dialog_model.dart';
 
 const double _graphicSize = 60;
@@ -26,90 +24,29 @@ class ResetPasswordDialog extends StackedView<ResetPasswordDialogModel> {
     ResetPasswordDialogModel viewModel,
     Widget? child,
   ) {
+    final width = MediaQuery.of(context).size.width;
     return Dialog(
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+      insetPadding: EdgeInsets.symmetric(horizontal: 40, vertical: 60),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
       backgroundColor: Colors.white,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Container(
-              decoration: BoxDecoration(color: darkGreenHeigh),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Expanded(
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          children: [
-                            Image.asset(myIcons.resetPassword, height: 30, width: 30,),
-                            Text(
-                              request.title ?? 'Reset Password',
-                              style: const TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.w900,
-                              ),
-                            ),
-                          ],
-                        ),
-                        if (request.description != null) ...[
-                          verticalSpaceTiny,
-                          Text(
-                            request.description!,
-                            style: const TextStyle(
-                              fontSize: 14,
-                              color: kcMediumGrey,
-                            ),
-                            maxLines: 3,
-                            softWrap: true,
-                          ),
-                        ],
-                      ],
-                    ),
-                  ),
-                  Container(
-                    width: _graphicSize,
-                    height: _graphicSize,
-                    decoration: const BoxDecoration(
-                      color: Color(0xFFF6E7B0),
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(_graphicSize / 2),
-                      ),
-                    ),
-                    alignment: Alignment.center,
-                    child: Image.asset(myIcons.cancel, height: 25, width: 25,)
-                  ),
-                ],
-              ),
-            ),
-            verticalSpaceMedium,
-            const LoginWithEmail(),
-            GestureDetector(
-              onTap: () => completer(DialogResponse(confirmed: true)),
-              child: Container(
-                height: 50,
-                width: double.infinity,
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-                  color: Colors.black,
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: const Text(
-                  'Got it',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 16,
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          SubBar(
+            height: 40,
+            color: Colors.white,
+            width: width * 1,
+            image: myIcons.filter,
+            text: 'Reset Password',
+            fontSize: 16,
+            fontWeight: FontWeight.w500,
+            btmLeftRadius: 0,
+            btmRightRadius: 0,
+            topimage: myIcons.cancel,
+            close: true,
+          ),
+          const ResetPassTab(),
+        ],
       ),
     );
   }
