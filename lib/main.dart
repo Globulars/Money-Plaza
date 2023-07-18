36148ab3588210c632d02f1724104f71316f912c
mudassir060 <mudassirmukhtar4@gmail.com>
2023-07-18 18:28:56 +0500

    change password

--- lib/main.dart
+++ lib/main.dart
@@ -40,7 +40,7 @@ class MyApp extends StatelessWidget {
       localizationsDelegates: context.localizationDelegates,
       supportedLocales: context.supportedLocales,
       locale: context.locale,
-      initialRoute: Routes.startupView,
+      initialRoute: Routes.changePasswordView,
       onGenerateRoute: StackedRouter().onGenerateRoute,
       navigatorKey: StackedService.navigatorKey,
       navigatorObservers: [
