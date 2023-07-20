6e4fb755712d7ab05d71f11a888817278fb1e5ba
Khawar Ayoub <124525015+Khawar101@users.noreply.github.com>
2023-07-20 13:35:23 +0500

    new

--- lib/ui/views/loan/applyconfirm/applyconfirm_viewmodel.dart
+++ lib/ui/views/loan/applyconfirm/applyconfirm_viewmodel.dart
@@ -1,4 +1,4 @@
-
+import 'package:money_plaza/app/app.router.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 
@@ -8,6 +8,6 @@ class ApplyconfirmViewModel extends BaseViewModel {
   final _navigationService = locator<NavigationService>();
 
   navigateToPloanreslut() {
-    _navigationService.back();
+    _navigationService.navigateToPloanreslutView();
   }
 }
