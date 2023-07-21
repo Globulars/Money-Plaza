e73c945373c7ac5b057c308b9593884e6156f012
mudassir060 <mudassirmukhtar4@gmail.com>
2023-07-21 22:22:25 +0500

    chnage

--- lib/ui/views/landing/startup/startup_viewmodel.dart
+++ lib/ui/views/landing/startup/startup_viewmodel.dart
@@ -8,7 +8,7 @@ class StartupViewModel extends BaseViewModel {
 
   // Place anything here that needs to happen before we get into the application
   Future runStartupLogic() async {
-    await Future.delayed(const Duration(seconds: 20));
+    await Future.delayed(const Duration(seconds: 3));
 
     _navigationService.replaceWithLandingView();
   }
