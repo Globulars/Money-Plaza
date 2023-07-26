ef6ec1fae30a06fcfd1af88fbdad89733b5866b5
Khawar Ayoub <124525015+Khawar101@users.noreply.github.com>
2023-07-26 20:21:38 +0500

    ..

--- lib/ui/views/loan/ownerloan/ownerloan_viewmodel.dart
+++ lib/ui/views/loan/ownerloan/ownerloan_viewmodel.dart
@@ -1,7 +1,8 @@
+// ignore_for_file: avoid_print
+
 import 'package:money_plaza/app/app.router.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
-
 import '../../../../app/app.locator.dart';
 
 class OwnerloanViewModel extends BaseViewModel {
