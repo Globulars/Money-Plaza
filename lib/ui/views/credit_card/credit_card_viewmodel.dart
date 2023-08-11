fed7a4df48f9fd4d8536d5ae75d2edda37fdfda9
mudassir060 <mudassirmukhtar4@gmail.com>
2023-08-11 16:55:50 +0500

    dropdowns

--- lib/ui/views/credit_card/credit_card_viewmodel.dart
+++ lib/ui/views/credit_card/credit_card_viewmodel.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:money_plaza/app/app.router.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
@@ -5,6 +6,33 @@ import '../../../app/app.locator.dart';
 
 class CreditCardViewModel extends BaseViewModel {
   final _navigationService = locator<NavigationService>();
+  String cardProvider = "Select the Card Providers";
+  String card = "Select type of Card";
+  final TextEditingController annualIncomeCtrl = TextEditingController();
+  List<String> cardProviderList = [
+    "Select the Card Providers",
+    "VISA",
+    "MASTER",
+    "Americen Express",
+    "UnionPay"
+  ];
+  List<String> cardList = [
+    "Select type of Card",
+    "Personal Card",
+    "Corporate Credit Card",
+    "Student Credit Card",
+    "Debit Card",
+  ];
+
+  setCardProvider(value) {
+    cardProvider = value;
+    notifyListeners();
+  }
+
+  setCard(value) {
+    card = value;
+    notifyListeners();
+  }
 
   navigateToCreditCardResult() {
     _navigationService.navigateToCreditResultView();
