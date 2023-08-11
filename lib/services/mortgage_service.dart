3c2bf717ff9c8681898eb936a5502653447e3964
Khawar Ayoub <124525015+Khawar101@users.noreply.github.com>
2023-08-11 17:51:37 +0500

    mortgages filled

--- lib/services/mortgage_service.dart
+++ lib/services/mortgage_service.dart
@@ -1,1 +1,28 @@
-class MortgageService {}
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '../ui/common/app_url.dart';
+
+class MortgageService {
+
+   final ApiUrl _apiUrl = ApiUrl();
+  Map<String, String> headers = {
+    "Accept": "application/json",
+    "content-type": "application/json"
+  };
+
+  mortgagesView(body) async {
+    try {
+      final response = await http.post(_apiUrl.mortgageList,
+          body: jsonEncode(body), headers: headers);
+      if (response.statusCode == 200) {
+        var data = json.decode(response.body);
+        return data;
+      } else {
+        return {"message": "${response.statusCode} error found"};
+      }
+    } catch (e) {
+      return {"message": e};
+    }
+  }
+
+}
