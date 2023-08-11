12c41f2ee86a6190c681d22235910fe322d331b4
Khawar Ayoub <124525015+Khawar101@users.noreply.github.com>
2023-08-11 11:43:30 +0500

    url

--- lib/ui/common/app_url.dart
+++ lib/ui/common/app_url.dart
@@ -3,8 +3,8 @@ const baseUrl = "https://api.moneyplaza.site";
 class ApiUrl {
 // authentication url
   final Uri login = Uri.parse("$baseUrl/auth/login");
-  final Uri loginByEmail = Uri.parse("$baseUrl/auth/loginByEmailCode");
-  final Uri loginByMobile = Uri.parse("$baseUrl/auth/loginByMobileCode");
+  final Uri loginByEmailCode = Uri.parse("$baseUrl/auth/loginByEmailCode");
+  final Uri loginByMobileCode = Uri.parse("$baseUrl/auth/loginByMobileCode");
   final Uri logOut = Uri.parse("$baseUrl/auth/logout");
   final Uri sendEmailCode = Uri.parse("$baseUrl/auth/sendEmailCode");
   final Uri sendSmsCode = Uri.parse("$baseUrl/auth/sendSmsCode");
