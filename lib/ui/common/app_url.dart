b836d75d6dea52afa27909267f44a8fe08ce3556
Khawar Ayoub <124525015+Khawar101@users.noreply.github.com>
2023-08-20 15:49:01 +0500

    url

--- lib/ui/common/app_url.dart
+++ lib/ui/common/app_url.dart
@@ -17,40 +17,40 @@ class ApiUrl {
 
   /////////////////////////////////////User Controller////////////////////////////////
 
-  final Uri getIncentives = Uri.parse("$baseUrl/user/getIncentives");
-  final Uri me = Uri.parse("$baseUrl/user/me");
-  final Uri query = Uri.parse("$baseUrl/user/query");
+  // final Uri getIncentives = Uri.parse("$baseUrl/user/getIncentives");
+  // final Uri me = Uri.parse("$baseUrl/user/me");
+  // final Uri query = Uri.parse("$baseUrl/user/query");
   final Uri sendForgetPasswordCodeByEmail =
       Uri.parse("$baseUrl/user/sendForgetPasswordCodeByEmail");
   final Uri sendForgetPasswordCodeByMobile =
       Uri.parse("$baseUrl/user/sendForgetPasswordCodeByMobile");
-  final Uri updateCreditCardDebtInformation =
-      Uri.parse("$baseUrl/user/updateCreditCardDebtInformation");
-  final Uri updateFinancialInformation =
-      Uri.parse("$baseUrl/user/updateFinancialInformation");
-  final Uri updateLoanInformation =
-      Uri.parse("$baseUrl/user/updateLoanInformation");
-  final Uri updateMortgageInformation =
-      Uri.parse("$baseUrl/user/updateMortgageInformation");
-  final Uri updatePassword = Uri.parse("$baseUrl/user/updatePassword");
+  // final Uri updateCreditCardDebtInformation =
+  //     Uri.parse("$baseUrl/user/updateCreditCardDebtInformation");
+  // final Uri updateFinancialInformation =
+  //     Uri.parse("$baseUrl/user/updateFinancialInformation");
+  // final Uri updateLoanInformation =
+  //     Uri.parse("$baseUrl/user/updateLoanInformation");
+  // final Uri updateMortgageInformation =
+  //     Uri.parse("$baseUrl/user/updateMortgageInformation");
+  // final Uri updatePassword = Uri.parse("$baseUrl/user/updatePassword");
   final Uri updatePasswordByEmailCode =
       Uri.parse("$baseUrl/user/updatePasswordByEmailCode");
   final Uri updatePasswordByMobileCode =
       Uri.parse("$baseUrl/user/updatePasswordByMobileCode");
-  final Uri updateProfile = Uri.parse("$baseUrl/user/updateProfile");
-  final Uri updateResidentialInformation =
-      Uri.parse("$baseUrl/user/updateResidentialInformation");
-  final Uri updateWorkInformation =
-      Uri.parse("$baseUrl/user/updateWorkInformation");
-  final Uri verifyEmail = Uri.parse("$baseUrl/user/verifyEmail");
-  final Uri verifyMobile = Uri.parse("$baseUrl/user/verifyMobile");
+  // final Uri updateProfile = Uri.parse("$baseUrl/user/updateProfile");
+  // final Uri updateResidentialInformation =
+  //     Uri.parse("$baseUrl/user/updateResidentialInformation");
+  // final Uri updateWorkInformation =
+  //     Uri.parse("$baseUrl/user/updateWorkInformation");
+  // final Uri verifyEmail = Uri.parse("$baseUrl/user/verifyEmail");
+  // final Uri verifyMobile = Uri.parse("$baseUrl/user/verifyMobile");
 
   /////////////////////////////////////account Controller////////////////////////////////
-  final Uri accountLike = Uri.parse("$baseUrl/account/like/");
-  final Uri accountList = Uri.parse("$baseUrl/account/list");
+  // final Uri accountLike = Uri.parse("$baseUrl/account/like/");
+  // final Uri accountList = Uri.parse("$baseUrl/account/list");
 
   /////////////////////////////////////award Controller////////////////////////////////
-  final Uri award = Uri.parse("$baseUrl/award");
+  // final Uri award = Uri.parse("$baseUrl/award");
 
   /////////////////////////////////////banner Controller////////////////////////////////
   final Uri bannerGetBannersByType =
