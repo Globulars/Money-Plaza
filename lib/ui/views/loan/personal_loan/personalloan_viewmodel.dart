e066f10815d601d6df57c8869d36e6edbe923c74
Khawar Ayoub <124525015+Khawar101@users.noreply.github.com>
2023-08-25 11:35:27 +0500

    textfield

--- lib/ui/views/loan/personal_loan/personalloan_viewmodel.dart
+++ lib/ui/views/loan/personal_loan/personalloan_viewmodel.dart
@@ -1,13 +1,29 @@
 // ignore_for_file: avoid_print
-
 import 'package:money_plaza/app/app.router.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
-
 import '../../../../app/app.locator.dart';
+import 'package:flutter/material.dart';
+
 
 class PersonalloanViewModel extends BaseViewModel {
   final _navigationService = locator<NavigationService>();
+
+  TextEditingController borrowingAmount =
+      TextEditingController(text: "50000");
+       String loanReason = "Business Expansion";
+      //  String loanReasonFOrApi = "Business Expansion";
+      String propertyOwner= "Have";
+  // String propertyOwnerForApi = "";
+
+   final loanReasonList = [
+    "Business Expansion",
+    "Car Purchase",
+    "Credit Card Repayment",
+    "Debt Consolidation",
+    "Car Purchase",
+    "Car Purchase",
+  ];
 
   var initialIndex = 0;
   var loanTenors = 6;
@@ -34,6 +50,10 @@ class PersonalloanViewModel extends BaseViewModel {
     outStanding = value;
     notifyListeners();
   }
+
+
+
+
 
   navigateToApplyconfirm() {
     _navigationService.navigateToApplyconfirmView(match: true);
