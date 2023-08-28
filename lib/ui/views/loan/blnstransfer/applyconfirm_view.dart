2397665963b30b9974b8cecb343dd58818ea3077
Khawar Ayoub <124525015+Khawar101@users.noreply.github.com>
2023-08-28 12:09:49 +0500

    blns transfer

---
+++ lib/ui/views/loan/blnstransfer/applyconfirm_view.dart
@@ -0,0 +1,116 @@
+// ignore_for_file: must_be_immutable
+import 'package:flutter/material.dart';
+import 'package:money_plaza/ui/views/loan/blnstransfer/blnstransfer_viewmodel.dart';
+import 'package:money_plaza/ui/widgets/common/icon_box_btn/return_button.dart';
+import 'package:money_plaza/ui/widgets/common/icon_box_btn/text.dart';
+import 'package:stacked/stacked.dart';
+import 'package:money_plaza/ui/common/app_icons.dart';
+import '../../../common/ui_helpers.dart';
+import '../../../widgets/app_bar.dart';
+import '../../../widgets/bottom_bar.dart';
+import '../../../widgets/common/background_image.dart';
+import '../../../widgets/common/custom_text_field/custom_text_field.dart';
+import '../../../widgets/common/icon_box_btn/sub_bar.dart';
+import '../../../widgets/common/icon_box_btn/submit_button.dart';
+
+
+class BlnstransferloanApplyConfirmView extends StackedView<BlnstransferViewModel> {
+  const BlnstransferloanApplyConfirmView({super.key});
+
+  @override
+  Widget builder(
+    BuildContext context,
+    BlnstransferViewModel viewModel,
+    Widget? child,
+  ) {
+    return Form(
+      key: viewModel.formKey,
+      child: Stack(
+        children: [
+          const BackgroundImage(),
+          Scaffold(
+            backgroundColor: Colors.transparent,
+            appBar: appBar(context),
+            body: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SubBar(
+                  divider: true,
+                  image: myIcons.contactInfo,
+                  text: 'contactInformation',
+                  height: 50,
+                  scale: 1.4,
+                  btmLeftRadius: 0,
+                  btmRightRadius: 0,
+                ),
+                verticalSpaceSmall,
+                Expanded(
+                  child: SingleChildScrollView(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10),
+                      child: Column(
+                        children: [
+                          CustomText(text: 'enterFollowingInformation'),
+                          verticalSpaceTiny,
+                          CustomTextField(
+                            titleText: 'fullName',
+                            controller: viewModel.fullNameCtrl,
+                          ),
+                          verticalSpaceTiny,
+                          CustomTextField(
+                            controller: viewModel.phoneNumberCtrl,
+                            textInputType: TextInputType.number,
+                            titleText: 'phoneNumber',
+                          ),
+                          verticalSpaceTiny,
+                          CustomTextField(
+                            controller: viewModel.emailCtrl,
+                            titleText: 'email',
+                          ),
+                          verticalSpaceTiny,
+                          CustomTextField(
+                            controller: viewModel.hkidCtrl,
+                            titleText: 'hkid',
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                verticalSpaceLarge
+              ],
+            ),
+          ),
+          bottomBar(
+            Row(
+              children: [
+                ReturnButton(
+                  imageLeft: myIcons.returnIcon1,
+                  imgwidth: 12,
+                  text: 'return',
+                  height: 40,
+                  width: 90,
+                ),
+                horizontalSpaceTiny,
+                SubmitButton(
+                  image: myIcons.done,
+                  imgwidth: 16,
+                  text: 'Done',
+                  height: 40,
+                  width: 80,
+                  onPress: viewModel.submitSurveyForm,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  BlnstransferViewModel viewModelBuilder(
+    BuildContext context,
+  ) =>
+      BlnstransferViewModel();
+}
--- lib/ui/views/loan/blnstransfer/blnstransfer_viewmodel.dart
+++ lib/ui/views/loan/blnstransfer/blnstransfer_viewmodel.dart
@@ -5,8 +5,14 @@ import 'package:money_plaza/app/app.router.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 import '../../../../app/app.locator.dart';
+import '../../../../services/loan_card_service.dart';
+import '../../../../services/toaster_service.dart';
 
 class BlnstransferViewModel extends BaseViewModel {
+  var formKey = GlobalKey<FormState>();
+
+  final _toasterService = locator<ToasterService>();
+  final _loanCardService = locator<LoanCardService>();
   final _navigationService = locator<NavigationService>();
   TextEditingController borrowingAmountCtrl =
       TextEditingController(text: "50000");
@@ -20,6 +26,13 @@ class BlnstransferViewModel extends BaseViewModel {
   TextEditingController remainigTenorCtrl = TextEditingController(text: "22");
   TextEditingController monthlyRepaymentCtrl =
       TextEditingController(text: "8000");
+  TextEditingController fullNameCtrl =
+      TextEditingController(text: "Mudassir Mukhtar");
+  TextEditingController phoneNumberCtrl =
+      TextEditingController(text: "3045655432");
+  TextEditingController emailCtrl =
+      TextEditingController(text: "mudassirmukhtar4@gmail.com");
+  TextEditingController hkidCtrl = TextEditingController(text: "SM6867864");
 
   String loanReason = "Business Expansion";
   String propertyOwner = "Have";
@@ -118,5 +131,178 @@ class BlnstransferViewModel extends BaseViewModel {
 
   navigateToBackScreen() {
     _navigationService.back();
+  }
+
+  navigateToOwnerloanresultView() {
+    _navigationService.navigateToOwnerloanresultView(body: {
+      "amount": borrowingAmountCtrl.text,
+      "tenor": loanTenors,
+      "type": "owner_loan",
+      "income": monthlyIncomeCtrl.text,
+      "currentTotalLoanAmount": totalOutstandingLoanCtrl.text,
+      "monthlyRepayment": monthlyRepaymentCtrl.text,
+      "pol": false
+    });
+  }
+
+  submitSurveyForm() async {
+    var isValid = formKey.currentState!.validate();
+    if (isValid) {
+      Map<String, dynamic> body = {
+        "result": [
+          {
+            "fieldName": "amount",
+            "fieldTitle": "借貸金額",
+            "fieldValue": borrowingAmountCtrl.text,
+            "fieldType": "text",
+            "fieldOrder": "",
+            "fieldAttrs": []
+          },
+          {
+            "fieldName": "tenor",
+            "fieldTitle": "還款期",
+            "fieldValue": loanTenors,
+            "fieldType": "text",
+            "fieldOrder": "",
+            "fieldAttrs": []
+          },
+          {
+            "fieldName": "reason",
+            "fieldTitle": "借貸原因",
+            "fieldValue": loanReason,
+            "fieldType": "text",
+            "fieldOrder": "",
+            "fieldAttrs": []
+          },
+          {
+            "fieldName": "income",
+            "fieldTitle": "每月收入",
+            "fieldValue": monthlyIncomeCtrl.text,
+            "fieldType": "text",
+            "fieldOrder": "",
+            "fieldAttrs": []
+          },
+          {
+            "fieldName": "salaryPayment",
+            "fieldTitle": "出糧方式",
+            "fieldValue": salaryPayment,
+            "fieldType": "text",
+            "fieldOrder": "",
+            "fieldAttrs": []
+          },
+          {
+            "fieldName": "employmentType",
+            "fieldTitle": "收入類型",
+            "fieldValue": typeOfIncome,
+            "fieldType": "text",
+            "fieldOrder": "",
+            "fieldAttrs": []
+          },
+          {
+            "fieldName": "incomeProofType",
+            "fieldTitle": "收入證明",
+            "fieldValue": proofOfIncome,
+            "fieldType": "text",
+            "fieldOrder": "",
+            "fieldAttrs": []
+          },
+          {
+            "fieldName": "hasLoan",
+            "fieldTitle": "現有未還清的貸款",
+            "fieldValue": "沒有",
+            "fieldType": "text",
+            "fieldOrder": "",
+            "fieldAttrs": []
+          },
+          // {
+          //   "fieldName": "remainingLoans",
+          //   "fieldTitle": "貸款數量",
+          //   "fieldValue": numOfLoansCtrl.text,
+          //   "fieldType": "totalLoanCount",
+          //   "fieldOrder": "",
+          //   "fieldAttrs": []
+          // },
+          {
+            "fieldName": "remainingLoanAmount",
+            "fieldTitle": "未償還貸款總額",
+            "fieldValue": totalOutstandingLoanCtrl.text,
+            "fieldType": "remainingLoanAmount",
+            "fieldOrder": "",
+            "fieldAttrs": []
+          },
+          {
+            "fieldName": "monthlyRepayment",
+            "fieldTitle": "每月還款",
+            "fieldValue": monthlyRepaymentCtrl.text,
+            "fieldType": "monthlyRepayment",
+            "fieldOrder": "",
+            "fieldAttrs": []
+          },
+          {
+            "fieldName": "propertyValue",
+            "fieldTitle": "物業價值",
+            "fieldValue": propertyOwner,
+            "fieldType": "text",
+            "fieldOrder": "",
+            "fieldAttrs": []
+          },
+          {
+            "fieldName": "propertyShare",
+            "fieldTitle": "現按成數",
+            "fieldValue": "0",
+            "fieldType": "text",
+            "fieldOrder": "",
+            "fieldAttrs": []
+          },
+          {
+            "fieldName": "fullname",
+            "fieldTitle": "英文全名",
+            "fieldValue": fullNameCtrl.text,
+            "fieldType": "name",
+            "fieldOrder": "",
+            "fieldAttrs": []
+          },
+          {
+            "fieldName": "mobile",
+            "fieldTitle": "聯絡電話",
+            "fieldValue": phoneNumberCtrl.text,
+            "fieldType": "mobile",
+            "fieldOrder": "",
+            "fieldAttrs": ["unique"]
+          },
+          {
+            "fieldName": "email",
+            "fieldTitle": "E-mail",
+            "fieldValue": emailCtrl.text,
+            "fieldType": "email",
+            "fieldOrder": "",
+            "fieldAttrs": []
+          },
+          {
+            "fieldName": "identity",
+            "fieldTitle": "身份證號碼",
+            "fieldValue": hkidCtrl.text,
+            "fieldType": "hkid",
+            "fieldOrder": "",
+            "fieldAttrs": ["unique"]
+          },
+          // {
+          //   "fieldName": "bornInHK",
+          //   "fieldTitle": "是否在香港出生?",
+          //   "fieldValue": "yes",
+          //   "fieldType": "text",
+          //   "fieldOrder": "",
+          //   "fieldAttrs": []
+          // }
+        ]
+      };
+      var data = await _loanCardService.ownerLoneSurveyform(body);
+      if (data["success"] == true) {
+        _toasterService.successToast(data["message"]);
+        navigateToOwnerloanresultView();
+      } else {
+        _toasterService.errorToast(data["message"].toString());
+      }
+    }
   }
 }
