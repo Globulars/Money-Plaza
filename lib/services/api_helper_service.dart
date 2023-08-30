c74254b2faa1bf0d016da6a34f7a041d145ed0e0
mudassir060 <mudassirmukhtar4@gmail.com>
2023-08-30 11:38:54 +0500

    upload image with mulity par api  reward applicat

--- lib/services/api_helper_service.dart
+++ lib/services/api_helper_service.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 import 'dart:developer';
+import 'dart:io';
 import 'package:http/http.dart' as http;
+import 'package:image_picker/image_picker.dart';
 import 'package:money_plaza/app/app.locator.dart';
 import 'package:money_plaza/services/auth_service.dart';
 
@@ -40,6 +42,42 @@ class ApiHelperService {
       if (response.statusCode == 200) {
         var data = json.decode(response.body);
         return data;
+      } else {
+        return {"message": "${response.statusCode} error found"};
+      }
+    } catch (e) {
+      return {"message": e};
+    }
+  }
+
+  uploadImageAndPost(body) async {
+    final ImagePicker _picker = ImagePicker();
+    final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
+    // File file = File(image!.path);
+    // return file;
+    return multiPartRequest(image!.path, body);
+  }
+
+  multiPartRequest(imagePath, body) async {
+    try {
+      var request = http.MultipartRequest("POST", Uri.parse("urlInsertImage"));
+      request.headers.addAll({
+        "Accept": "multipart/form-data",
+        "content-type": "multipart/form-data",
+        "Authorization": "Bearer $accessToken"
+      });
+      request.fields.addAll(body);
+      request.files.add(http.MultipartFile.fromBytes(
+          "files", File(imagePath).readAsBytesSync(),
+          filename: imagePath));
+      var response = await request.send();
+      if (response.statusCode == 200) {
+        // var data = json.decode(response.body);
+        log(response.statusCode.toString());
+        response.stream.transform(utf8.decoder).listen((value) {
+          log(value.toString());
+        });
+        return response;
       } else {
         return {"message": "${response.statusCode} error found"};
       }
--- lib/ui/views/member/reward_application/reward_application_view.dart
+++ lib/ui/views/member/reward_application/reward_application_view.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: non_constant_identifier_names, avoid_types_as_parameter_names, prefer_const_literals_to_create_immutables
 
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:money_plaza/ui/common/app_icons.dart';
 import 'package:money_plaza/ui/widgets/common/dropdown_textfield/model_dropdown.dart';
 import 'package:stacked/stacked.dart';
@@ -32,116 +34,124 @@ class RewardApplicationView extends StackedView<RewardApplicationViewModel> {
     Widget? child,
   ) {
     var width = MediaQuery.of(context).size.width;
-    return Stack(
-      children: [
-        const BackgroundImage(),
-        Scaffold(
-          backgroundColor: Colors.transparent,
-          appBar: appBar(context),
-          body: Column(
-            children: [
-              SubBar(
-                width: width * 0.95,
-                height: 50,
-                text: "moneyPlazaReward",
-                btmLeftRadius: 0,
-                btmRightRadius: 0,
-                fontSize: 16,
-              ),
-              const Divider(
-                color: darkGreenHeigh,
-                height: 1.0,
-                thickness: 3,
-              ),
-              Expanded(
-                child: SingleChildScrollView(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 20.0, vertical: 10.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        CustomText(
-                          text: "enterFollowingInformation",
-                          fontSize: 16,
-                          fontWeight: FontWeight.w500,
-                        ),
-                        verticalSpaceTiny,
-                        DropdownTextfield(
-                          titleText: 'typeOfProduct',
-                          onChanged: viewModel.setTypeOfProduct,
-                          options: viewModel.typeOfProductList,
-                          value: viewModel.typeOfProduct,
-                        ),
-                        verticalSpaceTiny,
-                        viewModel.institution != null
-                            ? ModelDropdown(
-                                value: viewModel.institution,
-                                titleText: 'institution',
-                                onChanged: viewModel.setInstitution,
-                                options: viewModel.institutionList,
-                              )
-                            : DropdownTextfield(
-                                titleText: 'institution',
-                                onChanged: (String) {},
-                                options: [],
-                              ),
-                        verticalSpaceTiny,
-                        viewModel.institution != null
-                            ? ModelDropdown(
-                                titleText: 'rewardDetails',
-                                onChanged: viewModel.setRewardDetails,
-                                options: viewModel.rewardDetailsList,
-                                value: viewModel.rewardDetails,
-                              )
-                            : DropdownTextfield(
-                                titleText: 'rewardDetails',
-                                onChanged: (String) {},
-                                options: [],
-                              ),
-                        verticalSpaceTiny,
-                        CustomTextField(
-                          controller: viewModel.referenceNumberCtrl,
-                          titleText: 'referenceNumber',
-                        ),
-                        verticalSpaceTiny,
-                        CustomTextField(
-                          titleText: 'referenceNumber',
-                          hintText: 'upload',
-                          textAlign: TextAlign.center,
-                        ),
-                        verticalSpaceLarge
-                      ],
+    return Form(
+      key: viewModel.formKey,
+      child: Stack(
+        children: [
+          const BackgroundImage(),
+          Scaffold(
+            backgroundColor: Colors.transparent,
+            appBar: appBar(context),
+            body: Column(
+              children: [
+                SubBar(
+                  width: width * 0.95,
+                  height: 50,
+                  text: "moneyPlazaReward",
+                  btmLeftRadius: 0,
+                  btmRightRadius: 0,
+                  fontSize: 16,
+                ),
+                const Divider(
+                  color: darkGreenHeigh,
+                  height: 1.0,
+                  thickness: 3,
+                ),
+                Expanded(
+                  child: SingleChildScrollView(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 20.0, vertical: 10.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          CustomText(
+                            text: "enterFollowingInformation",
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                          ),
+                          verticalSpaceTiny,
+                          DropdownTextfield(
+                            titleText: 'typeOfProduct',
+                            onChanged: viewModel.setTypeOfProduct,
+                            options: viewModel.typeOfProductList,
+                            value: viewModel.typeOfProduct,
+                          ),
+                          verticalSpaceTiny,
+                          viewModel.institution != null
+                              ? ModelDropdown(
+                                  value: viewModel.institution,
+                                  titleText: 'institution',
+                                  onChanged: viewModel.setInstitution,
+                                  options: viewModel.institutionList,
+                                )
+                              : DropdownTextfield(
+                                  titleText: 'institution',
+                                  onChanged: (String) {},
+                                  options: [],
+                                ),
+                          verticalSpaceTiny,
+                          viewModel.institution != null
+                              ? ModelDropdown(
+                                  titleText: 'rewardDetails',
+                                  onChanged: viewModel.setRewardDetails,
+                                  options: viewModel.rewardDetailsList,
+                                  value: viewModel.rewardDetails,
+                                )
+                              : DropdownTextfield(
+                                  titleText: 'rewardDetails',
+                                  onChanged: (String) {},
+                                  options: [],
+                                ),
+                          verticalSpaceTiny,
+                          CustomTextField(
+                            controller: viewModel.referenceNumberCtrl,
+                            titleText: 'referenceNumber',
+                          ),
+                          verticalSpaceTiny,
+                          Text(
+                            "uploadApplication",
+                            style: GoogleFonts.ibmPlexSans(
+                                fontSize: 14, fontWeight: FontWeight.w500),
+                          ).tr(),
+                          verticalSpaceTiny,
+                          ReturnButton(
+                            text: "Upload File",
+                            height: 40,
+                          ),
+                          verticalSpaceLarge
+                        ],
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
-        ),
-        bottomBar(
-          Row(
-            children: [
-              ReturnButton(
-                imageLeft: myIcons.returnIcon1,
-                imgwidth: 12,
-                text: 'return',
-                height: 40,
-                width: 80,
-              ),
-              horizontalSpaceTiny,
-              SubmitButton(
-                image: myIcons.done,
-                imgwidth: 12,
-                text: 'submit',
-                height: 40,
-                width: 80,
-              ),
-            ],
+          bottomBar(
+            Row(
+              children: [
+                ReturnButton(
+                  imageLeft: myIcons.returnIcon1,
+                  imgwidth: 12,
+                  text: 'return',
+                  height: 40,
+                  width: 80,
+                ),
+                horizontalSpaceTiny,
+                SubmitButton(
+                  image: myIcons.done,
+                  imgwidth: 12,
+                  text: 'submit',
+                  height: 40,
+                  width: 80,
+                ),
+              ],
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 
