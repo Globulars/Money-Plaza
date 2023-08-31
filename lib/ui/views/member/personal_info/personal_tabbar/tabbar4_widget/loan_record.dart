bcdf6ecc1d0a82955a8f795932e0a03e291cc2f1
Khawar Ayoub <124525015+Khawar101@users.noreply.github.com>
2023-08-31 14:22:25 +0500

    loan record

--- lib/ui/views/member/personal_info/personal_tabbar/tabbar4_widget/loan_record.dart
+++ lib/ui/views/member/personal_info/personal_tabbar/tabbar4_widget/loan_record.dart
@@ -26,127 +26,135 @@ class LoanRecordView extends StackedView<PersonalInfoViewModel> {
     PersonalInfoViewModel viewModel,
     Widget? child,
   ) {
-    final width = MediaQuery.of(context).size.width;   
-        return Stack(
-          children: [
-            Container(
-              width: width * 1,
-              height: MediaQuery.of(context).size.height * 1,
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage(myIcons.backgroundimage),
-                    fit: BoxFit.fill),
+    final width = MediaQuery.of(context).size.width;
+    return Stack(
+      children: [
+        Container(
+          width: width * 1,
+          height: MediaQuery.of(context).size.height * 1,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage(myIcons.backgroundimage), fit: BoxFit.fill),
+          ),
+          child: Scaffold(
+            backgroundColor: Colors.transparent,
+            appBar: appBar(context),
+            body: SingleChildScrollView(
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    CustomText(
+                      text: 'Loan Record',
+                      fontWeight: FontWeight.w600,
+                      fontSize: 18,
+                    ),
+                    verticalSpaceSmall,
+                    ModelDropdown(
+                      onChanged: viewModel.setLoanRecord,
+                      options: viewModel.loanRecordDataList,
+                      value: viewModel.loanRecordList,
+                      titleText: 'Type of Loan*',
+                    ),
+                    verticalSpaceTiny,
+                    CustomTextField(
+                      controller: viewModel.lenderCtrl,
+                      titleText: 'Lender*',
+                    ),
+                    verticalSpaceTiny,
+                    CustomTextField(
+                      controller: viewModel.outstandingCtrl,
+                      titleText: 'Outstanding*',
+                    ),
+                    verticalSpaceTiny,
+                    CustomTextField(
+                      controller: viewModel.loanRecordList!.code == "PLoan" ||
+                              viewModel.loanRecordList!.code ==
+                                  "PrepaidInterest" ||
+                              viewModel.loanRecordList!.code == "InterestOnly"
+                          ? viewModel.tenorCtrl
+                          : viewModel.monthlyInterestCtrl,
+                      titleText: viewModel.loanRecordList!.code == "PLoan" ||
+                              viewModel.loanRecordList!.code ==
+                                  "PrepaidInterest" ||
+                              viewModel.loanRecordList!.code == "InterestOnly"
+                          ? 'Tenor*'
+                          : "Monthly Interest",
+                    ),
+                    verticalSpaceTiny,
+                    CustomTextField(
+                      controller: viewModel.remainingTenorCtrl,
+                      titleText: 'Remaining Tenor*',
+                    ),
+                    verticalSpaceTiny,
+                    CustomTextField(
+                      controller: viewModel.monthlyRepaymentCtrl,
+                      titleText: 'Monthly Repayment*',
+                    ),
+                    // verticalSpaceTiny,
+                    // Row(
+                    //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    //   children: [
+                    //     ReturnButton(
+                    //       height: 41.5,
+                    //       text: 'm',
+                    //       width: width * 0.43,
+                    //       boxcolor:
+                    //           viewModel.gender == 'm' ? darkGreenHeigh : Colors.white,
+                    //       color:
+                    //           viewModel.gender != "m" ? darkGreenHeigh : Colors.white,
+                    //       onPress: () {
+                    //         viewModel.setGender('m');
+                    //       },
+                    //     ),
+                    //     ReturnButton(
+                    //       height: 41.5,
+                    //       text: 'f',
+                    //       width: width * 0.43,
+                    //       boxcolor:
+                    //           viewModel.gender == "f" ? darkGreenHeigh : Colors.white,
+                    //       color:
+                    //           viewModel.gender != "f" ? darkGreenHeigh : Colors.white,
+                    //       onPress: () {
+                    //         viewModel.setGender('f');
+                    //       },
+                    //     ),
+                    //   ],
+                    // ),
+                    verticalSpaceSmall,
+
+                    verticalSpaceLarge
+                  ],
+                ),
               ),
-              child: Scaffold(
-                backgroundColor: Colors.transparent,
-                appBar: appBar(context),
-                body:SingleChildScrollView(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-             CustomText(
-              text: 'Loan Record',
-              fontWeight: FontWeight.w600,fontSize: 18,
             ),
-            verticalSpaceSmall,
-            ModelDropdown(
-                    onChanged: viewModel.setLoanRecord,
-                    options: viewModel.loanRecordDataList,
-                    value: viewModel.loanRecordList,
-                    titleText: 'Type of Loan*',
-                  ),
-                  verticalSpaceTiny,
-            CustomTextField(
-              // controller: viewModel.firstNameCtrl,
-              titleText: 'Lender*',
-            ),
-            verticalSpaceTiny,
-            CustomTextField(
-              // controller: viewModel.lastNameCtrl,
-              titleText: 'Outstanding*',
-            ),
-            verticalSpaceTiny,
-           CustomTextField(
-              // controller: viewModel.lastNameCtrl,
-              titleText: 'Tenor*',
-            ),
-            verticalSpaceTiny,
-           CustomTextField(
-              // controller: viewModel.lastNameCtrl,
-              titleText: 'Remaining Tenor*',
-            ),
-            verticalSpaceTiny,
-           CustomTextField(
-              // controller: viewModel.lastNameCtrl,
-              titleText: 'Monthly Repayment*',
-            ),
-            // verticalSpaceTiny,
-            // Row(
-            //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            //   children: [
-            //     ReturnButton(
-            //       height: 41.5,
-            //       text: 'm',
-            //       width: width * 0.43,
-            //       boxcolor:
-            //           viewModel.gender == 'm' ? darkGreenHeigh : Colors.white,
-            //       color:
-            //           viewModel.gender != "m" ? darkGreenHeigh : Colors.white,
-            //       onPress: () {
-            //         viewModel.setGender('m');
-            //       },
-            //     ),
-            //     ReturnButton(
-            //       height: 41.5,
-            //       text: 'f',
-            //       width: width * 0.43,
-            //       boxcolor:
-            //           viewModel.gender == "f" ? darkGreenHeigh : Colors.white,
-            //       color:
-            //           viewModel.gender != "f" ? darkGreenHeigh : Colors.white,
-            //       onPress: () {
-            //         viewModel.setGender('f');
-            //       },
-            //     ),
-            //   ],
-            // ),
-            verticalSpaceSmall,
-           
-           
-            verticalSpaceLarge
-          ],
+          ),
         ),
-      ),
-    ),
+        bottomBar(
+          Row(
+            children: [
+              ReturnButton(
+                imageLeft: myIcons.returnIcon1,
+                imgwidth: 12,
+                text: 'return',
+                height: 40,
+                width: 80,
               ),
-            ),
-            bottomBar(
-              Row(
-                children: [
-                  ReturnButton(
-                    imageLeft: myIcons.returnIcon1,
-                    imgwidth: 12,
-                    text: 'return',
-                    height: 40,
-                    width: 80,
-                  ),
-                  horizontalSpaceTiny,
-                  SubmitButton(
-                    image: myIcons.done,
-                    imgwidth: 16,
-                    text: 'Submit',
-                    height: 40,
-                    width: 80,
-                  ),
-                ],
+              horizontalSpaceTiny,
+              SubmitButton(
+                image: myIcons.done,
+                imgwidth: 16,
+                text: 'Submit',
+                height: 40,
+                width: 80,
               ),
-            ),
-          ],
-        );
-     
-   
+            ],
+          ),
+        ),
+      ],
+    );
   }
 
   @override
