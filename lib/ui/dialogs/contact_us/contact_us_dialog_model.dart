353d162710eba50409697b5129c8718e23b06563
mudassir060 <mudassirmukhtar4@gmail.com>
2023-08-31 13:16:31 +0500

    urlLaunch for testing

--- lib/ui/dialogs/contact_us/contact_us_dialog_model.dart
+++ lib/ui/dialogs/contact_us/contact_us_dialog_model.dart
@@ -3,8 +3,29 @@ import 'package:url_launcher/url_launcher.dart';
 
 class ContactUsDialogModel extends BaseViewModel {
   Future<void> urlLaunch(_url) async {
-  if (!await launchUrl(Uri.parse(_url))) {
-    throw Exception('Could not launch $_url');
+    if (!await launchUrl(Uri.parse(_url))) {
+      throw Exception('Could not launch $_url');
+    }
   }
-}
+
+  Future<void> whatsappsms() async {
+    final Uri _url = Uri.parse("whatsapp://send?phone=+93454335400");
+    if (await canLaunchUrl(_url)) {
+      await launchUrl(_url);
+    } else {}
+  }
+
+  Future<void> simsms() async {
+    final Uri _url = Uri.parse("sms:03454335400");
+    if (await canLaunchUrl(_url)) {
+      await launchUrl(_url);
+    }
+  }
+
+  Future<void> call() async {
+    final Uri _url = Uri.parse("tel:+1-555-010-999");
+    if (await canLaunchUrl(_url)) {
+      await launchUrl(_url);
+    }
+  }
 }
