6dd820b2befd29dcb08ce45bc00f25abaa30cad2
Khawar Ayoub <124525015+Khawar101@users.noreply.github.com>
2023-09-05 15:53:28 +0500

    d

--- lib/ui/dialogs/credit_card_filter/credit_card_filter_dialog.dart
+++ lib/ui/dialogs/credit_card_filter/credit_card_filter_dialog.dart
@@ -1,4 +1,3 @@
-// ignore_for_file: non_constant_identifier_names, avoid_types_as_parameter_names, prefer_const_literals_to_create_immutables
 import 'package:flutter/material.dart';
 import 'package:money_plaza/ui/common/app_colors.dart';
 import 'package:money_plaza/ui/common/ui_helpers.dart';
